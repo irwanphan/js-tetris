87167fbec1e2db691b65c7f3712413386ac5f644
irwanphan <irwanphan@gmail.com>
2020-05-29 11:21:17 +0700

    tetromino is piling up~

--- app.js
+++ app.js
@@ -86,6 +86,18 @@ document.addEventListener('DOMContentLoaded', () => {
         undraw()
         currentPosition += width
         draw()
+        freeze()
     }
 
+    // freeze
+    function freeze() {
+        if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
+            current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+            // start a new falling tetromino
+            random = Math.floor(Math.random()*theTetrominoes.length)
+            current = theTetrominoes[random][currentRotation]
+            currentPosition = 4
+            draw()
+        }
+    }
 })
